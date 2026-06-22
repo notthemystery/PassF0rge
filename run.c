8393d75313e99b6e7db64bc8001a3997fcf3c537
The Mystery <132539789+notthemystery@users.noreply.github.com>
2026-06-22 16:39:00 +0530

    Update run.c

--- run.c
+++ run.c
@@ -3,34 +3,19 @@
 
 #ifdef _WIN32
     #define EXPORT __declspec(dllexport)
-
-    static void set_env(const char *name, const char *value)
-    {
-        _putenv_s(name, value);
-    }
-
 #else
     #define EXPORT __attribute__((visibility("default")))
-
-    static void set_env(const char *name, const char *value)
-    {
-        setenv(name, value, 1);
-    }
-
 #endif
+char cmd[256];
 
 EXPORT int run_linux(const char *username, const char *password)
 {
-    set_env("USERNAME", username);
-    set_env("PASSWORD", password);
-
-    return system("./Exploits/Linux/CopyFail") == 0 ? 0 : -1;
+    snprintf(cmd, sizeof(cmd), "./Exploits/Linux/CopyFail %s %s", username, password);
+    return system(cmd) == 0 ? 0 : -1;
 }
 
 EXPORT int run_windows(const char *username, const char *password)
 {
-    set_env("USERNAME", username);
-    set_env("PASSWORD", password);
-
-    return system("Exploits\\Windows\\RedSun.exe") == 0 ? 0 : -1;
+    snprintf(cmd, sizeof(cmd), "Exploits\\Windows\\RedSun.exe %s %s", username, password);
+    return system(cmd) == 0 ? 0 : -1;
 }
