d38c4f2b900db3421a6db68c55fcb83af77c34d2
The Mystery <132539789+notthemystery@users.noreply.github.com>
2026-06-22 18:45:33 +0530

    Implement Windows run function with environment variables
    Added Windows command execution and environment variable setup.

--- run.c
+++ run.c
@@ -5,8 +5,8 @@
     #define EXPORT __declspec(dllexport)
 #else
     #define EXPORT __attribute__((visibility("default")))
+    char cmd[256];
 #endif
-char cmd[256];
 
 EXPORT int run_linux(const char *username, const char *password)
 {
@@ -14,12 +14,15 @@ EXPORT int run_linux(const char *username, const char *password)
     return system(cmd) == 0 ? 0 : -1;
 }
 
+
 EXPORT int run_windows(const char *username, const char *password)
 {
+#ifdef _WIN32
     _putenv_s("USERNAME", username);
     _putenv_s("PASSWORD", password);
     system("Exploits\\Windows\\RedSun.exe") == 0 ? 0 : -1;
     _putenv_s("USERNAME", "");
     _putenv_s("USERNAME", "");
+#endif
     return 0;
 }
